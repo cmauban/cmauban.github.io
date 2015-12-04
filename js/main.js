37813b1f7ee22eaf944b6f6abbcf841a9ce65381
Camille Mauban <camille.mauban@gmail.com>
2015-12-03 21:01:58 -0500

    hamburger nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
+$('.hamburger').on('click', function(){
+  $('.hamburger-nav').slideToggle('show');
+});
+
+// // hamburger menu behavior
+// var hb=false;
+// $('.hamburger').on('click', function(){
+//   if (hb === false) {
+//   $('.hamburger-nav').slideToggle('show');
+//   setTimeout(function(){ hb=true; }, 500);
+// }
+// });
+//
+// $('.hamburger-nav li').on('click', function(){
+//   $('.hamburger-nav').slideToggle('show');
+//   hb=false;
+// });
+
+// $('*:not(.hamburger)').on('click', function(){
+//   if (hb) {
+//     $('.hamburger-nav').slideToggle('show');
+//     hb=false;
+//   }
+// });
+
+
+
 $('.nav').each(function(){ //iterates through each ahref
   $('.main-content').each(function(){ //iterates through the corresponding content
 
-  $('a').on('click', function(event){
+  $('.nav a').on('click', function(event){
     // event.preventDefault(); //stop browser to take action for clicked
 
     $(this).addClass('active').siblings().removeClass('active'); //current href clicked is active
