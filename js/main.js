9da7a6970072e9c50d4d2ec3e3429cd087f0897e
Camille Mauban <camille.mauban@gmail.com>
2016-01-18 13:12:38 -0500

    combined watercolor and digital illustaions for the time being

--- js/main.js
+++ js/main.js
@@ -49,6 +49,10 @@ $('#slider').click(function(e) {
     e.preventDefault();
 });
 
+// $('.slider2 .control_next2').on('click', function(e) {
+//     e.preventDefault();
+// });
+
   $('#checkbox').change(function(){
     setInterval(function () {
         moveRight();
@@ -96,49 +100,51 @@ $('#slider').click(function(e) {
 
 
 // slider 2
-    $('.slider2').click(function(e) {
-        e.preventDefault();
-    });
-    //
-    //   $('#checkbox').change(function(){
-    //     setInterval(function () {
-    //         moveRight();
-    //     }, 3000);
-    //   });
-    //
-    // 	var slideCount = $('.slider2 ul li').length;
-    // 	var slideWidth = $('.slider2 ul li').width();
-    // 	var slideHeight = $('.slider2 ul li').height();
-    // 	var sliderUlWidth = slideCount * slideWidth;
-    //
-    // 	$('.slider2').css({ width: slideWidth, height: slideHeight });
-    //
-    // 	$('.slider2 ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-    //
-    //     $('.slider2 ul li:last-child').prependTo('.slider2 ul');
-    //
-    //     function moveLeft() {
-    //         $('.slider2 ul').animate({
-    //             left: + slideWidth
-    //         }, 200, function () {
-    //             $('.slider2 ul li:last-child').prependTo('.slider2 ul');
-    //             $('.slider2 ul').css('left', '');
-    //         });
-    //     }
-    //
-    //     function moveRight() {
-    //         $('.slider2 ul').animate({
-    //             left: - slideWidth
-    //         }, 200, function () {
-    //             $('.slider2 ul li:first-child').appendTo('.slider2 ul');
-    //             $('.slider2 ul').css('left', '');
-    //         });
-    //     }
-    //
-    //     $('a.control_prev2').click(function () {
-    //         moveLeft();
-    //     });
-    //
-    //     $('a.control_next2').click(function () {
-    //         moveRight();
-    //     });
+    // $('.slider2').on('click', function(e) {
+    //     e.preventDefault();
+    // });
+
+      // $('#checkbox').change(function(){
+      //   setInterval(function () {
+      //       moveRight();
+      //   }, 3000);
+      // });
+      //
+    	// var slideCount = $('.slider2 ul li').length;
+    	// var slideWidth = $('.slider2 ul li').width();
+    	// var slideHeight = $('.slider2 ul li').height();
+    	// var sliderUlWidth = slideCount * slideWidth;
+      //
+    	// $('.slider2').css({ width: slideWidth, height: slideHeight });
+      //
+    	// $('.slider2 ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+      //
+      //   $('.slider2 ul li:last-child').prependTo('.slider2 ul');
+      //
+      //   function moveLeft() {
+      //       $('.slider2 ul').animate({
+      //           left: + slideWidth
+      //       }, 200, function () {
+      //           $('.slider2 ul li:last-child').prependTo('.slider2 ul');
+      //           $('.slider2 ul').css('left', '');
+      //       });
+      //   }
+      //
+      //   function moveRight() {
+      //       $('.slider2 ul').animate({
+      //           left: - slideWidth
+      //       }, 200, function () {
+      //           $('.slider2 ul li:first-child').appendTo('.slider2 ul');
+      //           $('.slider2 ul').css('left', '');
+      //       });
+      //   }
+      //
+      //   $('a.control_prev2').click(function () {
+      //       e.preventDefault();
+      //       moveLeft();
+      //   });
+      //
+      //   $('a.control_next2').click(function () {
+      //       e.preventDefault();
+      //       moveRight();
+      //   });
